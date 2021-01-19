0759b9668897cf34cd41112cf56712347b475268
laurafroelich <lafr@dhigroup.com>
2021-01-19 13:56:13 +0100

    Increase minimum required version for dash, plotly, and dash-bootstrap-components

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="anomalydetection_app",
     version='0.0.1',
-    install_requires=["dash>=1.16", "plotly>=4.12", "pandas>=1.1", "dash-bootstrap-components>=0.10",
+    install_requires=["dash>=1.18", "plotly>=4.14", "pandas>=1.1", "dash-bootstrap-components>=0.11",
                       "anomalydetection", "numpy>=1.19"],
     dependency_links=["https://github.com/DHI/anomalydetection.git"],
     extras_require={
